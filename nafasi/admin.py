73f11599f849e57a1551131d094dbae5bf002594
seballos <sebas11cs@gmail.com>
2023-07-15 22:39:09 -0400

    chore(Admin): improve Position fields and add TabularInline to add skills

--- nafasi/admin.py
+++ nafasi/admin.py
@@ -1,8 +1,19 @@
 from django.contrib import admin
 from .models import Position, Skill, ExperienceWeight, Vacancy
 
+class ExperienceWeightInline(admin.TabularInline):
+    model = ExperienceWeight
+    extra = 1
+    autocomplete_fields = ['skill']
+
+class SkillAdmin(admin.ModelAdmin):
+    inlines = (ExperienceWeightInline,)
+    search_fields = ('name',)
+
+class PositionAdmin(admin.ModelAdmin):
+    inlines = (ExperienceWeightInline,)
+
 # Register your models here.
-admin.site.register(Position)
-admin.site.register(ExperienceWeight)
-admin.site.register(Skill)
+admin.site.register(Position, PositionAdmin)
+admin.site.register(Skill, SkillAdmin)
 admin.site.register(Vacancy)
