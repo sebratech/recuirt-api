19e691fea230fe921087f7af99ff9a52febcf2c0
seballos <sebas11cs@gmail.com>
2023-11-19 20:16:04 -0500

    feat: implement update positions

--- nafasi/utils.py
+++ nafasi/utils.py
@@ -9,3 +9,12 @@ def set_serializer_author_and_response(cls, request):
         serializer.save()
         return Response(serializer.data, status=status.HTTP_201_CREATED)
     return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+def set_serializer_author_response_update(cls, request, instance):
+    data = request.data
+    data['author']  = request.user.id
+    serializer = cls.serializer_class(instance, data=data)
+    if(serializer.is_valid()):
+        serializer.save()
+        return Response(serializer.data, status=status.HTTP_200_OK)
+    return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
